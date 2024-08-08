ba143078da658317dd035e292f9e6b6b83022ad3
Sai Vathsa Kondepudi <vathsakondepudi@gmail.com>
2024-08-08 14:59:59 +0530

    after adding e

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import find_packages,setup
+#from typing import List
+
+def get_requirements()->list[str]:
+    requirements_list = list[str] =[]
+
+    return requirements_list
+
+
+setup(
+    name='sensor',
+    version="0.0.1",
+    author="Sai Vathsa Kondepudi",
+    author_email="vathsakondepudi@gmail.com",
+    packages = find_packages(),
+    install_requires=get_requirements()
+)
